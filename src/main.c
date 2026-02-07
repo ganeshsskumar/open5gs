f7e0295f59ffe49cd42f5df89dc5ba8f45215ccb
Ganesh kumar <85495411+ganeshsskumar@users.noreply.github.com>
2026-02-07 13:49:06 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -108,9 +108,9 @@ int main(int argc, const char *const argv[])
      *
      * Keep the order of starting-up
      */
-setbuf(stdout, NULL);
-setbuf(stderr, NULL);
-printf("IN MAIN FUNCTION\n");
+//setbuf(stdout, NULL);
+//setbuf(stderr, NULL);
+//printf("IN MAIN FUNCTION\n");
     
 #ifdef ENABLE_LICENSE_CHECK
    INT32 status = LICENSE_VOID;
@@ -124,8 +124,8 @@ printf("IN MAIN FUNCTION\n");
     }
 #endif
     
-setbuf(stdout, NULL);
-setbuf(stderr, NULL);
+//setbuf(stdout, NULL);
+//setbuf(stderr, NULL);
     
     int rv, i, opt;
     ogs_getopt_t options;
